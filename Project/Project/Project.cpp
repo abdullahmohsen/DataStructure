aacd0ae6206446b0e4321bc38511c15b4eb01fed
abdullahmohsen <44713320+abdullahmohsen@users.noreply.github.com>
2020-07-03 19:30:50 +0200

    test

--- Project/Project/Project.cpp
+++ Project/Project/Project.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "Node.h"
+#include "LinkedList.h"
 using namespace std;
 
 int main()
 {
+    LinkedList list;
+    list.append(7);
+    list.append(1);
+    list.append(9);
+    list.print();
 
     return 0;
 }
