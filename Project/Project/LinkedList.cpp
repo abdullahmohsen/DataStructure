30adf74c6a8ff71bca34a8d23a67d28a68ea9e17
abdullahmohsen <44713320+abdullahmohsen@users.noreply.github.com>
2020-07-03 19:34:59 +0200

    Insert fead

--- Project/Project/LinkedList.cpp
+++ Project/Project/LinkedList.cpp
@@ -1,5 +1,6 @@
 #include "LinkedList.h"
 #include <iostream>
+#include <assert.h>
 using namespace std;
 
 //Put a newNode in the last List
@@ -20,6 +21,31 @@ void LinkedList::append(int val)
 		currentNode->next = newNode; //Next of currentNode equal newNode
 	}
 	size++; //when I make a newNode the number of nodes in the list will be increases 
+}
+
+//Put a newNode in the any index in the List
+void LinkedList::insert(int index, int val)
+{
+	assert(index >= 0 && index <= size); //Runtime error if the condition is false (index != negative)
+	Node* newNode = new Node(val); //Create a newNode
+	if (index == 0) //Put a newNode in the place of the head
+	{
+		newNode->next = head; //Next of newNode equal head
+		head = newNode;
+	}
+	else
+	{
+		Node* currentNode = head; //currentNode is a pointer points on head
+		int i = 0; //i is the index of nodes in the list
+		while (i < index - 1)
+		{
+			currentNode = currentNode->next;
+			i++;
+		}
+		newNode->next = currentNode->next; //next of newNode equal next of currentNode
+		currentNode->next = newNode; //next of currentNode equal newNode
+	}
+	size++;
 }
 
 //Print values of currentNode 
